770a657b31c10390c7648bc077b21ea447af2016
syd <nmmilk@163.com>
2017-09-25 17:50:02 +0800

    0.0.1

--- modules/app/cooperation.js
+++ modules/app/cooperation.js
@@ -18,7 +18,7 @@ var mySwiper = new Swiper("#swiper1", {
     watchSlidesProgress: !0,
     onProgress: function (a) {
         var b, c, d;
-        for (b = 0; b < a.slides.length; b++) c = a.slides[b], d = c.progress, scale = 1 - Math.min(Math.abs(.2 * d), 1), es = c.style, es.opacity = 1 - Math.min(Math.abs(d / 2), 1), es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform = "translate3d(0px,0," + -Math.abs(150 * d) + "px) scale("+scale+")"
+        for (b = 0; b < a.slides.length; b++) c = a.slides[b], d = c.progress, es = c.style, es.opacity =c.hasClass("swiper-slide-active")?1: 1 - Math.min(Math.abs(d / 2), 1), es.scale =(1 - Math.min(Math.abs(.2 * d), 1)), es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform = "translate3d(0px,0," + -Math.abs(150 * d) + "px) scale("+es.scale+","+es.scale+")"
     },
     onSetTransition: function (a, b) {
         for (var c = 0; c < a.slides.length; c++) es = a.slides[c].style, es.webkitTransitionDuration = es.MsTransitionDuration = es.msTransitionDuration = es.MozTransitionDuration = es.OTransitionDuration = es.transitionDuration = b + "ms"
